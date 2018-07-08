ab9973b68e3c1d5621343ba64ee82c79378811e2
huangjianxiong <huangjianxiong@ainemo.com>
2018-07-08 13:21:24 +0800

    update clone_demos.bat

--- clone_demos.bat
+++ clone_demos.bat
@@ -7,4 +7,23 @@ COLOR 1f
 cd ..
 mkdir soui-demo
 cd soui-demo
+git clone https://github.com/soui-demo/UiEditor.git
+git clone https://github.com/soui-demo/qqlogin.git
+git clone https://github.com/soui-demo/360.git
+git clone https://github.com/soui-demo/360Preview.git
+git clone https://github.com/soui-demo/souispy.git
+git clone https://github.com/soui-demo/PcManager.git
+git clone https://github.com/soui-demo/SoTool.git
+git clone https://github.com/soui-demo/mclistview_demo.git
 git clone https://github.com/soui-demo/souitest.git
+git clone https://github.com/soui-demo/VUI.git
+git clone https://github.com/soui-demo/BesLyric.git
+git clone https://github.com/soui-demo/QQMain.git
+git clone https://github.com/soui-demo/FrogPlay.git
+git clone https://github.com/soui-demo/MultiLangs.git
+git clone https://github.com/soui-demo/TXMgr.git
+git clone https://github.com/soui-demo/SLogViewer.git
+git clone https://github.com/soui-demo/souieditor.git
+git clone https://github.com/soui-demo/DropDemo.git
+git clone https://github.com/soui-demo/ThreeClearGame.git
+git clone https://github.com/soui-demo/baiduwp.git
