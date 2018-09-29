ad0ceac835347a5d78474aa2cc9485df3eb8d89b
setoutsoft <setoutsoft@qq.com>
2018-09-29 22:01:21 +0800

    update build_rc2.bat

--- demo/build_rc2.bat
+++ demo/build_rc2.bat
@@ -1,1 +1,1 @@
-..\..\tools\uiresbuilder.exe -i "uires\uires.idx" -p uires -r .\res\soui_res.rc2 -h .\res\resource.h idtable
+..\tools\uiresbuilder.exe -i "uires\uires.idx" -p uires -r .\res\soui_res.rc2 -h .\res\resource.h idtable
