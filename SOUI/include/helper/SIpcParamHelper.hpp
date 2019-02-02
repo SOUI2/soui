12e78ab1e1491e5b006577746d5048d8a6fb2fb7
setoutsoft <setoutsoft@qq.com>
2019-02-02 22:52:47 +0800

    update SIpcParamHelper.hpp

--- SOUI/include/helper/SIpcParamHelper.hpp
+++ SOUI/include/helper/SIpcParamHelper.hpp
@@ -5,7 +5,7 @@ enum{FUN_ID=id};\
 UINT GetID() {return FUN_ID;}
 
 #define FUN_BEGIN \
-bool HandleFun(UINT uMsg, SParamStream &ps){ \
+bool HandleFun(UINT uMsg, SOUI::SParamStream &ps){ \
 	bool bHandled = false; \
 
 #define FUN_HANDLER(x,fun) \
@@ -14,7 +14,7 @@ bool HandleFun(UINT uMsg, SParamStream &ps){ \
 		x param; \
 		param.FromStream4Input(ps);\
 		fun(param); \
-		SParamStream psOut(ps.GetBuffer(),true);\
+		SOUI::SParamStream psOut(ps.GetBuffer(),true);\
 		param.ToStream4Output(psOut);\
 		bHandled = true;\
 	}
@@ -36,83 +36,83 @@ bool HandleFun(UINT uMsg, SParamStream &ps){ \
 
 /////////////////////////////////////////////////////////////////////
 template<typename P1>
-void toParamStream(SParamStream &  ps, P1 &p1)
+void toParamStream(SOUI::SParamStream &  ps, P1 &p1)
 {
 	ps << p1;
 }
 template<typename P1>
-void fromParamStream(SParamStream &  ps, P1 & p1)
+void fromParamStream(SOUI::SParamStream &  ps, P1 & p1)
 {
 	ps >> p1;
 }
 
 #define PARAMS1(type,p1) \
-void ToStream4##type(SParamStream &  ps){ toParamStream(ps,p1);}\
-void FromStream4##type(SParamStream &  ps){fromParamStream(ps,p1);}\
+void ToStream4##type(SOUI::SParamStream &  ps){ toParamStream(ps,p1);}\
+void FromStream4##type(SOUI::SParamStream &  ps){fromParamStream(ps,p1);}\
 
 /////////////////////////////////////////////////////////////
 template<typename P1, typename P2>
-void toParamStream(SParamStream &  ps, P1 &p1, P2 & p2)
+void toParamStream(SOUI::SParamStream &  ps, P1 &p1, P2 & p2)
 {
 	ps << p1 << p2;
 }
 template<typename P1, typename P2>
-void fromParamStream(SParamStream &  ps, P1 & p1, P2 &p2)
+void fromParamStream(SOUI::SParamStream &  ps, P1 & p1, P2 &p2)
 {
 	ps >> p1 >> p2;
 }
 
 #define PARAMS2(type,p1,p2) \
-void ToStream4##type(SParamStream &  ps){ toParamStream(ps,p1,p2);}\
-void FromStream4##type(SParamStream &  ps){fromParamStream(ps,p1,p2);}\
+void ToStream4##type(SOUI::SParamStream &  ps){ toParamStream(ps,p1,p2);}\
+void FromStream4##type(SOUI::SParamStream &  ps){fromParamStream(ps,p1,p2);}\
 
 
 ////////////////////////////////////////////////////////////////////
 template<typename P1, typename P2, typename P3>
-void toParamStream(SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3)
+void toParamStream(SOUI::SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3)
 {
 	ps << p1 << p2 << p3;
 }
 template<typename P1, typename P2, typename P3>
-void fromParamStream(SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3)
+void fromParamStream(SOUI::SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3)
 {
 	ps >> p1 >> p2 >> p3;
 }
 
 #define PARAMS3(type,p1,p2,p3) \
-void ToStream4##type(SParamStream &  ps){ toParamStream(ps,p1,p2,p3);}\
-void FromStream4##type(SParamStream &  ps){fromParamStream(ps,p1,p2,p3);}\
+void ToStream4##type(SOUI::SParamStream &  ps){ toParamStream(ps,p1,p2,p3);}\
+void FromStream4##type(SOUI::SParamStream &  ps){fromParamStream(ps,p1,p2,p3);}\
 
 ///////////////////////////////////////////////////////////////////
 template<typename P1, typename P2, typename P3, typename P4>
-void toParamStream(SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3, P4 & p4)
+void toParamStream(SOUI::SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3, P4 & p4)
 {
 	ps << p1 << p2 << p3<<p4;
 }
 template<typename P1, typename P2, typename P3, typename P4>
-void fromParamStream(SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3, P4 & p4)
+void fromParamStream(SOUI::SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3, P4 & p4)
 {
 	ps >> p1 >> p2 >> p3>>p4;
 }
 
 #define PARAMS4(type,p1,p2,p3,p4) \
-void ToStream4##type(SParamStream &  ps){ toParamStream(ps,p1,p2,p3,p4);}\
-void FromStream4##type(SParamStream &  ps){fromParamStream(ps,p1,p2,p3,p4);}\
+void ToStream4##type(SOUI::SParamStream &  ps){ toParamStream(ps,p1,p2,p3,p4);}\
+void FromStream4##type(SOUI::SParamStream &  ps){fromParamStream(ps,p1,p2,p3,p4);}\
 
 
 /////////////////////////////////////////////////////////////////////////
 template<typename P1, typename P2, typename P3, typename P4, typename P5>
-void toParamStream(SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3, P4 & p4, P5 &p5)
+void toParamStream(SOUI::SParamStream &  ps, P1 &p1, P2 & p2, P3 & p3, P4 & p4, P5 &p5)
 {
 	ps << p1 << p2 << p3 << p4 <<p5;
 }
 template<typename P1, typename P2, typename P3, typename P4, typename P5>
-void fromParamStream(SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3, P4 & p4, P5 &p5)
+void fromParamStream(SOUI::SParamStream &  ps, P1 & p1, P2 &p2, P3 & p3, P4 & p4, P5 &p5)
 {
 	ps >> p1 >> p2 >> p3 >> p4>>p5;
 }
 
 #define PARAMS5(type,p1,p2,p3,p4,p5) \
-void ToStream4##type(SParamStream &  ps){ toParamStream(ps,p1,p2,p3,p4,p5);}\
-void FromStream4##type(SParamStream &  ps){fromParamStream(ps,p1,p2,p3,p4,p5);}\
+void ToStream4##type(SOUI::SParamStream &  ps){ toParamStream(ps,p1,p2,p3,p4,p5);}\
+void FromStream4##type(SOUI::SParamStream &  ps){fromParamStream(ps,p1,p2,p3,p4,p5);}\
 
