f8ad3673dc022c4a281580e92a12070a4f5564df
008 <359501300@qq.com>
2019-03-29 19:57:22 +0800

    clone_demos.bat 添加判断pro文件

--- clone_demos.bat
+++ clone_demos.bat
@@ -66,9 +66,10 @@ if(%all%=="2") (
 goto inipro
 
 :inputfilelist
-for /f "tokens=* eol=." %%a in ('dir /ad /b') do (   
-  Echo SUBDIRS += %%a>>soui-demo.pro
- )
+for /f "tokens=* eol=." %%a in ('dir /ad /b') do (  
+if exist %%a\%%a.pro  Echo SUBDIRS += %%a>>soui-demo.pro
+)
+
 goto :createbat
 :error
 Echo "error 有一些错误请检查前面的输出"
